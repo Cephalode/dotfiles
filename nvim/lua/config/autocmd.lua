e1051219392a245f3a89fb8c5174509a7b892a0d
Cephalode <git@cephalode.com>
2025-12-15 18:25:23 -0500

    added nvim config

--- nvim/lua/config/autocmd.lua
+++ nvim/lua/config/autocmd.lua
@@ -1,6 +1,3 @@
--- autocmd
---------------------------------------------------------------------------------
--- Highlight when yanking
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
