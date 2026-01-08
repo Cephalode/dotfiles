bc057c99cba9f9b588a7668ec1179d4a4605f6fb
Cephalode <git@cephalode.com>
2026-01-07 23:44:30 -0500

    ci: 📦 metasepia update

--- nvim/init.lua
+++
@@ -1,9 +0,0 @@
-require "config.autocmd"
-require "config.globals"
-require "config.keymap"
-require "config.lsp"
-require "config.options"
-require "config.theme"
-
-require "plugins.oil"
-require "plugins.treesitter"
